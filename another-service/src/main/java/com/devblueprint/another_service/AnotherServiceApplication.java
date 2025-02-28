25d34c2b2bd09aedadd9ae4f56e553306a14b749
Your Name <nguyenducanhtuan0602@gmail.com>
2025-02-28 19:46:01 +0700

    add another Service

---
+++ another-service/src/main/java/com/devblueprint/another_service/AnotherServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devblueprint.another_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotherServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotherServiceApplication.class, args);
+	}
+
+}
---
+++ another-service/src/test/java/com/devblueprint/another_service/AnotherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devblueprint.another_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
