eb5d829761290a6380c3cdf92abab47caf060bfb
Phong Vo <phong.vo27ams@hcmut.edu.vn>
2025-04-05 21:48:29 +0700

    Use mapper for updating appointment; Handle null input

--- scheduling-service/src/main/java/com/devteria/doctor_service/mapper/AppointmentMapper.java
+++ scheduling-service/src/main/java/com/devteria/doctor_service/mapper/AppointmentMapper.java
@@ -2,12 +2,14 @@ package com.devteria.doctor_service.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import com.devteria.doctor_service.dto.request.AppointmentRequest;
 import com.devteria.doctor_service.dto.response.AppointmentResponse;
 import com.devteria.doctor_service.entity.Appointment;
 
-@Mapper(componentModel = "spring")
+// Only non-null values will be updated
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface AppointmentMapper {
     Appointment toAppointment(AppointmentRequest request);
 
